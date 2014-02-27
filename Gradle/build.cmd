38dbdc99d9819b1495f15b9c3e406a69258f0dd4
artbear <aartbear@gmail.com>
2014-02-27 21:28:06 +0400

    +тестирование работы в платформах 8.2. и 8.3, и режимах обычное/управляемое приложение

--- Gradle/build.cmd
+++ Gradle/build.cmd
@@ -1,6 +1,6 @@
 @echo off
-chcp 1251
+chcp 1251 >nul
 REM 
-@gradle --info -q xUnitFor1C
+@gradle --info -q xUnitFor1C_82 xUnitFor1C_82_Managed
 REM gradle --info -Dfile.encoding=utf-8 -q xUnitFor1C
 rem @pause
