474cae79edb793303fec368f23d14aa716555ff3
artbear <aartbear@gmail.com>
2014-02-27 22:04:45 +0400

    шаг тестирования снова зависит только от настроек

--- Gradle/build.cmd
+++ Gradle/build.cmd
@@ -1,6 +1,6 @@
 @echo off
 chcp 1251 >nul
 REM 
-@gradle --info -q xUnitFor1C_82 xUnitFor1C_82_Managed
+@gradle --info -q xUnitFor1C
 REM gradle --info -Dfile.encoding=utf-8 -q xUnitFor1C
 rem @pause
