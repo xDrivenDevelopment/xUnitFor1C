26b6a6dc3a24a0a2d2ae1f9cea9809671ecfc45f
Сосна <shenja@sosna.zp.ua>
2014-09-16 22:35:03 +0300

    Исправленна строка запуска тестового клиента, в linux имеет значение пробелы между параметрами строки запуска.

---
+++ Tests/runtests.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/sh
+
+WORKSPACE=`pwd`
+
+if test -d $WORKSPACE/build; then rm -rf $WORKSPACE/build;fi
+mkdir $WORKSPACE/build -p
+
+if test -d $WORKSPACE/test-reports; then rm -rf $WORKSPACE/test-reports;fi
+mkdir $WORKSPACE/test-reports/ordinary -p
+
+if test -d /opt/1C/v8.3/x86_64; then 
+	oneC_root=/opt/1C/v8.3/x86_64
+else 
+	oneC_root=/opt/1C/v8.3/i386
+fi
+
+
+echo "create database $oneC_root"
+$oneC_root/1cv8 CREATEINFOBASE  File=$WORKSPACE/build/ib/ /Lru
+echo "resotore base"
+$oneC_root/1cv8 DESIGNER /F$WORKSPACE/build/ib/ /Lru /RestoreIB $WORKSPACE/Tests/TestBase.dt
+echo "load cf"
+$oneC_root/1cv8 DESIGNER /F$WORKSPACE/build/ib/ /Nadmin /Lru /LoadCfg$WORKSPACE/Tests/TestConfig.cf /UpdateDBCfg
+
+echo "run thick client ordinary mode"
+echo "$oneC_root/1cv8" ENTERPRISE /Lru /F"$WORKSPACE/build/ib/" /Nadmin /C"xddRun;$WORKSPACE/Tests/;xddReportFormat;xml;xddExitCodePath;$WORKSPACE/out.txt;xddReportPath;$WORKSPACE/test-reports/thick.xml;" /Execute"$WORKSPACE/xddTestRunner.epf" /RunModeOrdinary /outrunTest.txt
+run = "$oneC_root/1cv8" ENTERPRISE /Lru /F"$WORKSPACE/build/ib/" /Nadmin /C"xddRun;$WORKSPACE/Tests/;xddReportFormat;xml;xddExitCodePath;$WORKSPACE/out.txt;xddReportPath;$WORKSPACE/test-reports/thick.xml;" /Execute"$WORKSPACE/xddTestRunner.epf" /RunModeOrdinary /outrunTest.txt
+echo "$run"
+run
+"$oneC_root/1cv8c" ENTERPRISE /Lru /F"$WORKSPACE/build/ib/" /Nadmin /C"xddRun;$WORKSPACE/Tests/ManagedApp/;xddReportFormat;xml;xddExitCodePath;$WORKSPACE/out.txt;xddReportPath;$WORKSPACE/test-reports/thin_Managedapp.xml;" /Execute"$WORKSPACE/src/xddTestRunner.epf" /outrunTest.txt  /TESTMANAGER
+
+"$oneC_root/1cv8c" ENTERPRISE /Lru /F"$WORKSPACE/build/ib/" /Nadmin /C"xddRun;$WORKSPACE/src/Tests/Gui83/;xddReportFormat;xml;xddExitCodePath;$WORKSPACE/out.txt;xddReportPath;$WORKSPACE/test-reports/thin_GUI.xml;" /Execute"$WORKSPACE/src/xddTestRunner.epf" /outrunTest.txt  /TESTMANAGER
+
+"$oneC_root/1cv8" ENTERPRISE /Lru /F"$WORKSPACE/build/ib/" /Nadmin /C"xddRun;$WORKSPACE/src/Tests/;xddReportFormat;xml;xddExitCodePath;$WORKSPACE/out.txt;xddReportPath;$WORKSPACE/test-reports/thick_all.xml;" /Execute"$WORKSPACE\src\xddTestRunner.epf" /outrunTest.txt /RunModeManagedApplication /TESTMANAGER
