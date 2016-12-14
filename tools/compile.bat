f67d201b045454ff30f45733b4fc71ee86a6497f
Artur Ayukhanov <aartbear@gmail.com>
2016-12-14 23:56:03 +0300

    При компиляции и декомпиляции обработок используем доступ к базе через пользователя-администратора

--- tools/compile.bat
+++ tools/compile.bat
@@ -9,14 +9,17 @@ SETLOCAL
 set BUILDPATH=%projectdir%\build
 rem if not exist %BUILDPATH% set BUILDPATH=..\build
 
-set RUNNER_IBNAME=/F"%BUILDPATH%\ib"
+rem set RUNNER_IBNAME=/F"%BUILDPATH%\ib"
 rem set RUNNER_DBUSER=base
 rem set RUNNER_DBPWD=234567890
+SET USERPWD=--db-user admin
 
 rem 
 SET RUNNER_ENV=production
 
+SET connstring=--ibname /F"%BUILDPATH%\ib" %USERPWD%
+
 echo "compileepf"
 rem oscript -encoding=utf-8 %mypath%/runner.os compileepf .\ %BUILDPATH%\out\
-oscript %mypath%/runner.os compileepf %BUILDPATH%\.. %BUILDPATH%\out\
+oscript %mypath%/runner.os compileepf %BUILDPATH%\.. %BUILDPATH%\out\ %connstring% 
 exit /B
--- tools/decompile.bat
+++ tools/decompile.bat
@@ -6,15 +6,15 @@ SET mypath=C:\projects\vanessa-runner\tools
 echo %mypath%
 
 rem SET USERPWD=--db-user base --db-pwd 234567890
-SET connstring=
-SET USERPWD=
+rem SET connstring=
+SET USERPWD=--db-user admin
 rem 
 SET RUNNER_ENV=production
 
 set BUILDPATH=%projectdir%\build
 rem if not exist %BUILDPATH% set BUILDPATH=..\build
 
-SET connstring=--ibname /F"%BUILDPATH%\ib"
+SET connstring=--ibname /F"%BUILDPATH%\ib" %USERPWD%
 
 @rem oscript -encoding=utf-8 %mypath%/runner.os decompileepf %BUILDPATH%\out\ %BUILDPATH%\..\src %connstring% 
 oscript %mypath%/runner.os decompileepf %BUILDPATH%\out\ %BUILDPATH%\.. %connstring% 
