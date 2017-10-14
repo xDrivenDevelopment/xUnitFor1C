2bd0a486e715a30589ddb8ac72f08cb03c37881d
Artur Ayukhanov <aartbear@gmail.com>
2017-10-15 00:19:49 +0300

    Восстановлены правильные команды для локального прогона #738

--- test.cmd
+++ test.cmd
@@ -5,7 +5,7 @@
 
 @set RUNNER_IBCONNECTION=/F./build/ibservice
 
-@REM @call runner init-dev --src src/tests/cf/83
+@call runner init-dev --src src/tests/cf/83
 
 @IF ERRORLEVEL 1 goto error
 
@@ -13,7 +13,7 @@
 
 @REM @IF ERRORLEVEL 1 goto error
 
-@REM @call runner run --command СоздатьАдминистратора
+@call runner run --command СоздатьАдминистратора
 
 @IF ERRORLEVEL 1 goto error
 
@@ -24,7 +24,7 @@
 @echo .
 @call runner xunit --settings tools/vrunner.json
 
-@call allure generate ./build/out/allure
+@call allure generate ./build/out/allure --clean
 
 @IF ERRORLEVEL 1 goto error
 
